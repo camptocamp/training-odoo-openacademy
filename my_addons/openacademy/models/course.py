5bf0b3ee209174fd89e9fe070491bac71d9f1c3a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-10-17 23:25:52 +0200

    Exercice 4.1 : Création de modèle

---
+++ my_addons/openacademy/models/course.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text()
