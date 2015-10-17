93dcbe297c5d87c9222e9483947c4ab76ddfb055
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-10-17 23:50:19 +0200

    Exercice 7.1 : relations Many2one

--- my_addons/openacademy/models/course.py
+++ my_addons/openacademy/models/course.py
@@ -6,3 +6,7 @@ class Course(models.Model):
 
     name = fields.Char(string='Title', required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one(comodel_name='res.users',
+                                     ondelete='set null',
+                                     string='Responsible',
+                                     index=True)
--- my_addons/openacademy/models/session.py
+++ my_addons/openacademy/models/session.py
@@ -8,3 +8,9 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one(comodel_name='res.partner',
+                                    string='Instructor')
+    course_id = fields.Many2one(comodel_name='openacademy.course',
+                                ondelete='cascade',
+                                string='Course',
+                                required=True)
