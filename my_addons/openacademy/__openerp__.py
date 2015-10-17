d21d0700620205ff1f53f95d5367bc8e7727ed39
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-10-17 23:20:06 +0200

    Exercice 3.1 : Créer un module

---
+++ my_addons/openacademy/__openerp__.py
@@ -0,0 +1,20 @@
+{'name': 'Open Academy',
+ 'author': 'Vous!',
+ 'version': '1.0',
+ 'category': 'Tools',
+ 'summary': 'Courses, Sessions, Subscriptions',
+ 'description': """,
+Open Academy
+============
+
+Open Academy module for managing trainings:
+- training courses
+- training sessions
+- attendees registration
+ """,
+ 'depends': ['base'],
+ 'data': [],
+ 'images': [],
+ 'demo': [],
+ 'application': True,
+ }
