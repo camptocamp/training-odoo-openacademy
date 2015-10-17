d524857e303c35fe5b9487702e2a8a2c72d5212b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-10-18 00:18:52 +0200

    Exercice bonus 12.3 : corriger la méthode copy

--- my_addons/openacademy/models/course.py
+++ my_addons/openacademy/models/course.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -22,3 +22,17 @@ class Course(models.Model):
          'UNIQUE(name)',
          "The course title must be unique"),
     ]
+
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
