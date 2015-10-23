d9427344fe8a90e7472581eda6cc46978c278f58
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-10-23 10:05:50 +0200

    Exercice 12.1 : ajouter une contrainte Python

--- my_addons/openacademy/models/session.py
+++ my_addons/openacademy/models/session.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -31,6 +31,14 @@ class Session(models.Model):
                 r.taken_seats = 0.0
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError(
+                    "A session's instructor can't be an attendee"
+                )
 
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
